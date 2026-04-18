8e68a0ec4228f94f3334888aeb918c46c49c56a6
Larissa <larissa.silva@lps.ufrj.br>
2026-04-18 13:48:29 -0300

    missao 2: histogramas das variaveis cineticas

--- scripts/print_dataframe.py
+++ scripts/print_dataframe.py
@@ -1,5 +1,42 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-dr = pd.read_csv('subset.csv')
+def printDataframe(dataframe):
+    df = pd.read_csv(dataframe)
+    print(df)
 
-print(dr)
+def plotHistogram(variable,dataframe):
+    df = pd.read_csv(dataframe)
+    if variable == 'et':
+        data= df['TrigEMClusterContainer.et']
+    elif variable == 'eta':
+        data= df['TrigEMClusterContainer.eta']
+    elif variable == 'phi':
+        data = df['TrigEMClusterContainer.phi']
+    elif variable == 'avgmu':
+        data = df['EventInfoContainer.avgmu']
+    
+    if variable == 'et':
+        plt.hist(data, bins=50 ,color = 'lightblue', edgecolor = 'black') 
+        plt.xlabel(r'$E_T$')
+        plt.title(r'Distribuição de $E_T$')
+    elif variable == 'eta':
+        plt.hist(data, bins=50 ,color = 'lightblue', edgecolor = 'black') 
+        plt.xlabel(r'$\eta$')
+        plt.title(r'Distribuição de $\eta$')
+    elif variable == 'phi':
+        plt.hist(data, bins=50 ,color = 'lightblue', edgecolor = 'black') 
+        plt.xlabel(r'$\phi$')
+        plt.title(r'Distribuição de $\phi$')
+    elif variable == 'avgmu':
+        plt.hist(data, bins=100 ,color = 'lightblue', edgecolor = 'black') 
+        plt.xlabel(r'$avg(\mu)$')
+        plt.title(r'Distribuição de $avg(\mu)$')
+    plt.ylabel('frequency')
+    plt.show()
+
+print(plotHistogram('et','subset.csv'))
+print(plotHistogram('eta','subset.csv'))
+print(plotHistogram('phi','subset.csv'))
+print(plotHistogram('avgmu','subset.csv'))
